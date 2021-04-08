34bfec5fe90e4efb1b77adbc35709484dbc4f767
joão Luís <jolitp@gmail.com>
2021-04-08 12:50:21 -0300

    adds module template

---
+++ src/templates/module_template.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+"""
+    helper functions for use in various scripts
+"""
+
+def function(parameter : str,
+    debug_function : bool = False):
+    """
+        docstring for function
+
+    Args:
+        root_directory (str):
+
+    Returns:
+        str:
+    """
+# region def function(...)
+
+# region debug_function
+    if debug_function:
+        print("[def] function(")
+        print("      parameter : str = {1}"\
+            .format(str(parameter)))
+        print("              )")
+        print("{")
+# endregion
+
+
+
+# region debug_function
+    if debug_function:
+        print("}")
+# endregion
+
+
+    return
+# endregion def function(...)
+
+
+
+if __name__ == '__main__':
+    print("module_template.py.__main__")
+    print()
+    function(" a string ",debug_function=True)
