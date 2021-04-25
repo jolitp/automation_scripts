5eaea56659e088f0c7740f4250efca8aa01d7200
João Luís <jolitp@gmail.com>
2021-04-25 15:50:26 -0300

    tests get all videos with a text file inside directory

--- src/multiple_files_operations/get_all_videos_in_a_directory/tests/integration_tests_get_all_videos_in_a_directory.py
+++ src/multiple_files_operations/get_all_videos_in_a_directory/tests/integration_tests_get_all_videos_in_a_directory.py
@@ -81,6 +81,64 @@ class IntegrationTest_get_all_videos_in_a_directory(unittest.TestCase):
 
     # endregion def test_folders_with_only_videos_should_return_only_videos(...):
 
+    # region def (...):
+    def test_folders_with_videos_and_a_text_file_should_return_only_videos(self):
+        """when giving a directory to get_all_videos(...)
+            with videos and a text file inside
+            should return only the videos
+        """
+
+        # setup
+        # cSpell:disable
+        project_folder = "/home/jolitp/Projects/automation_scripts/"
+        # cSpell:enable
+        tests_folder = \
+            "src/multiple_files_operations/get_all_videos_in_a_directory/tests/"
+        test_bed_folder = "test_bed/"
+        this_test_folder = \
+        "test_folders_with_videos_and_a_text_file_should_return_only_videos/"
+
+        root_folder = project_folder + tests_folder + test_bed_folder + this_test_folder
+
+        inputs = [
+            root_folder + "only_one_video/",
+            root_folder + "two_videos/",
+            root_folder + "three_videos/",
+
+        ]
+
+        # cSpell: disable
+        expected_values = [
+            # expected_values[0]
+            [root_folder + "only_one_video/dummy_video.mkv"],
+            # expected_values[1]
+            [root_folder + "two_videos/dummy_video_01.mp4",
+            root_folder + "two_videos/dummy_video_02.webm"],
+            # expected_values[2]
+            [root_folder + "three_videos/dummy_video_1.avi",
+            root_folder + "three_videos/dummy_video_2.rmvb",
+            root_folder + "three_videos/dummy_video_3.wmv",]
+        ]
+        # cSpell: enable
+
+        # act
+        for index, _ in enumerate(expected_values):
+            with self.subTest():
+                param : str = inputs[index]
+                expected : list = expected_values[index]
+                result = get_all_videos_in_a_directory_script\
+                    .get_all_videos(param,debug_function=DEBUG_TEST)
+                # self.assertEqual(result, expected)
+        # assert
+                self.assertCountEqual(result, expected)
+                ...
+            ...
+
+
+        ...
+    # endregion def (...):
+
+
 
     # region def (...):
     def test_folders_with_videos_and_a_directory_should_return_only_videos(self):
