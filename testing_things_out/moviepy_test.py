a70edc0ba2d45e8fada8924b42e66aca185551bd
João Luís <jolitp@gmail.com>
2021-05-20 22:50:04 -0300

    updates todos

--- testing_things_out/moviepy_test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-
-clip_1 = VideoFileClip("1.mp4")
-clip_2 = VideoFileClip("2.mp4")
-final_clip = concatenate_videoclips([clip_1,clip_2])
-final_clip.write_videofile("final.mp4")
